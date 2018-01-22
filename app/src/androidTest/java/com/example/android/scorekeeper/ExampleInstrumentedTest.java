1020a28d4fa605b2e9fc2873b42a923b1152caca
jbcruz <35581373+juleskoderepo@users.noreply.github.com>
2018-01-21 16:20:32 -0800

    Minor cosmetic and project changes
    Project was created from a copy but some package references did not
    change when it was refactored. Minor change to header on home screen.

--- app/src/androidTest/java/com/example/android/scorekeeper/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/android/scorekeeper/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.android.courtcounter", appContext.getPackageName());
+        assertEquals("com.example.android.scorekeeper", appContext.getPackageName());
     }
 }
